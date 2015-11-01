4962bca2c8231de694ecef6624e47c6a0b10e97d
Gujarat Santana <gujarat.santana@gmail.com>
2015-11-02 04:50:33 +0700

    header fix not perfect

--- app/src/main/java/santana/tebaktebakan/activity/MainActivity.java
+++ app/src/main/java/santana/tebaktebakan/activity/MainActivity.java
@@ -1,12 +1,22 @@
 package santana.tebaktebakan.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Slide;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 import santana.tebaktebakan.R;
 import santana.tebaktebakan.adapter.MainActivityAdapter;
@@ -18,11 +28,14 @@ public class MainActivity extends AppCompatActivity{
 
     protected RecyclerView list_level;
     AppCompatButton btnMenu1;
+    private CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initActivityTransitions();
         setContentView(R.layout.layout_main);
+
 
 
         initUI();
@@ -43,9 +56,57 @@ public class MainActivity extends AppCompatActivity{
         list_level = (RecyclerView)findViewById(R.id.list_level);
         btnMenu1 = (AppCompatButton) findViewById(R.id.btnMenu1);
 
+        supportPostponeEnterTransition();
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
+
+        final ImageView image = (ImageView) findViewById(R.id.headerIcon);
+        Picasso.with(this).load(R.drawable.anti_boring).into(image, new Callback() {
+            @Override
+            public void onSuccess() {
+                Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+                Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+                    public void onGenerated(Palette palette) {
+                        applyPalette(palette);
+                    }
+                });
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
+
+
         LinearLayoutManager llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
         list_level.setLayoutManager(llm);
         list_level.setAdapter(new MainActivityAdapter(getApplicationContext(),this,R.layout.layout_item_ganjil));
     }
+
+    private void initActivityTransitions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide transition = new Slide();
+            transition.excludeTarget(android.R.id.statusBarBackground, true);
+            getWindow().setEnterTransition(transition);
+            getWindow().setReturnTransition(transition);
+        }
+    }
+
+    private void applyPalette(Palette palette) {
+        int primaryDark = getResources().getColor(R.color.primary);
+        int primary = getResources().getColor(R.color.primary);
+        collapsingToolbarLayout.setContentScrimColor(palette.getMutedColor(primary));
+        collapsingToolbarLayout.setStatusBarScrimColor(palette.getDarkMutedColor(primaryDark));
+        supportStartPostponedEnterTransition();
+    }
+
+//    private void updateBackground(FloatingActionButton fab, Palette palette) {
+//        int lightVibrantColor = palette.getLightVibrantColor(getResources().getColor(android.R.color.white));
+//        int vibrantColor = palette.getVibrantColor(getResources().getColor(R.color.accent));
+//
+//        fab.setRippleColor(lightVibrantColor);
+//        fab.setBackgroundTintList(ColorStateList.valueOf(vibrantColor));
+//    }
 }
