a4d1cfc643c5ffb613444decc55500d218859eb2
Adrian Eka Fikri Pratono <adrianekafikri@gmail.com>
2015-11-01 16:07:10 +0700

    adrian initial commit

---
+++ app/src/main/java/santana/tebaktebakan/activity/MainActivityBaru.java
@@ -0,0 +1,17 @@
+package santana.tebaktebakan.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import santana.tebaktebakan.R;
+
+/**
+ * Created by AdrianEkaFikri on 11/1/2015.
+ */
+public class MainActivityBaru extends AppCompatActivity{
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_baru);
+    }
+}
