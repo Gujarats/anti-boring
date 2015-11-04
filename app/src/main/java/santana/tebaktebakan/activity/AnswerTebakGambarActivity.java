d739a92a5b8a47973cfeb9f52d631e3ec5bf8485
Adrian Eka Fikri Pratono <adrianekafikri@gmail.com>
2015-11-04 14:03:41 +0700

    penambahan layout tebak_semua_gambar trus di link ke ChooseImageTebakanActivity
    semua Activity sudah ke link ke layout masing2

--- app/src/main/java/santana/tebaktebakan/activity/AnswerTebakGambarActivity.java
+++ app/src/main/java/santana/tebaktebakan/activity/AnswerTebakGambarActivity.java
@@ -1,9 +1,18 @@
 package santana.tebaktebakan.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import santana.tebaktebakan.R;
 
 /**
  * Created by Gujarat Santana on 01/11/15.
  */
 public class AnswerTebakGambarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_answer_tebak_gambar);
+    }
 }
--- app/src/main/java/santana/tebaktebakan/activity/ChooseImageTebakanActivity.java
+++ app/src/main/java/santana/tebaktebakan/activity/ChooseImageTebakanActivity.java
@@ -1,9 +1,19 @@
 package santana.tebaktebakan.activity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import santana.tebaktebakan.R;
 
 /**
  * Created by Gujarat Santana on 01/11/15.
  */
 public class ChooseImageTebakanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_tebak_semua_gambar);
+    }
 }
