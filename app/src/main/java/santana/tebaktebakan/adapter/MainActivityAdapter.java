b3b90db1e0faffbe81351eb2d9bdfe4f8520bb3e
Gujarat Santana <gujarat.santana@gmail.com>
2015-11-08 10:44:41 +0700

    header item fix bugs

--- app/src/main/java/santana/tebaktebakan/adapter/MainActivityAdapter.java
+++ app/src/main/java/santana/tebaktebakan/adapter/MainActivityAdapter.java
@@ -2,6 +2,7 @@ package santana.tebaktebakan.adapter;
 
 import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,7 +11,6 @@ import android.widget.ImageView;
 
 import santana.tebaktebakan.R;
 import santana.tebaktebakan.session.SessionLevel;
-import view.CostumFont;
 
 /**
  * Created by AdrianEkaFikri on 11/1/2015.
@@ -27,8 +27,6 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
         this.context = context;
         this.activity = activity;
         this.layout = layout;
-        sessionLevel = new SessionLevel(context);
-        sessionLevel.setLevel(0);
     }
 
     @Override
@@ -41,17 +39,8 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-
-        if(position==0){
-            sessionLevel.setLevel(0);
-        }
-        lvl = sessionLevel.getLevel();
-        System.out.println("LVL " + lvl);
-
-        holder.txtLevel.setText(lvl);
-
-        sessionLevel.setLevel(lvl);
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        holder.txtLevel.setText(String.valueOf(position + 1));
 
     }
 
@@ -60,21 +49,17 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
         return 40;
     }
 
-    private int increment() {
-        lvl = lvl + 1;
-        return lvl;
-    }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public view.CostumFont txtLevel;
+        public AppCompatTextView txtLevel;
         public ImageView bintang1, bintang2;
 
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            txtLevel = (CostumFont) view.findViewById(R.id.txtLvl);
+            txtLevel = (AppCompatTextView) view.findViewById(R.id.txtLvl);
             bintang1 = (ImageView) view.findViewById(R.id.bintang1);
             bintang2 = (ImageView) view.findViewById(R.id.bintang2);
 
