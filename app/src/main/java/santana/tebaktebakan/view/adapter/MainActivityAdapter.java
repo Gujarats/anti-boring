0da08b71a9eafb027bd392319db8ef5893c6af5c
Gujarat Santana <gujarat.santana@gmail.com>
2015-12-13 20:09:52 +0700

    add fitur pointer bintang

--- app/src/main/java/santana/tebaktebakan/view/adapter/MainActivityAdapter.java
+++ app/src/main/java/santana/tebaktebakan/view/adapter/MainActivityAdapter.java
@@ -3,15 +3,16 @@ package santana.tebaktebakan.view.adapter;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.AppCompatTextView;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import santana.tebaktebakan.R;
 import santana.tebaktebakan.view.activity.StageActivity;
@@ -40,16 +41,12 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, final int position) {
-        initPosition(position,holder.txtLevel);
-        holder.layoutLevel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context, StageActivity.class);
-                intent.putExtra("LEVEL_STAGE" , position+1);
-                ActivityCompat.startActivity(activity, intent, Bundle.EMPTY);
-            }
-        });
+    public void onBindViewHolder( ViewHolder holder, int position) {
+        initPosition(position, holder.txtLevel);
+        setLevel(position,holder.layoutLevel);
+        setBintangLvl(position,holder.bintang1,holder.bintang2);
+
+
     }
 
     private void initPosition(int position, AppCompatTextView txtLevel){
@@ -58,6 +55,51 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
             txtLevel.setText("0"+lvl);
         } else {
             txtLevel.setText(""+lvl);
+        }
+    }
+
+    private void setBintangLvl(int position,ImageView bintang1,ImageView bintang2){
+        int positionLvlUser = 3;
+        int stageComplete =1;
+
+        if(position == positionLvlUser-1){
+            if(stageComplete==1){
+                // bintang 1
+                bintang1.setVisibility(View.VISIBLE);
+                bintang2.setVisibility(View.INVISIBLE);
+            }else if(stageComplete==2){
+                // bintang 2
+                bintang1.setVisibility(View.VISIBLE);
+                bintang2.setVisibility(View.VISIBLE);
+            }
+        }else{
+            bintang1.setVisibility(View.VISIBLE);
+            bintang2.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    private void setLevel(int position,LinearLayout layoutCompat){
+        int positionLvlUser = 3;
+        if(position < positionLvlUser){
+            final int pos = position+1;
+            layoutCompat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(context, StageActivity.class);
+                    intent.putExtra("LEVEL_STAGE" , pos);
+                    ActivityCompat.startActivity(activity, intent, Bundle.EMPTY);
+                }
+            });
+            layoutCompat.setBackgroundColor(Color.parseColor("#303F9F"));
+        }else{
+            layoutCompat.setBackgroundColor(Color.parseColor("#999999"));
+            layoutCompat.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
         }
     }
 
@@ -71,13 +113,13 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
         public final View mView;
         public AppCompatTextView txtLevel;
         public ImageView bintang1, bintang2;
-        public LinearLayoutCompat layoutLevel;
+        public LinearLayout layoutLevel;
 
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            layoutLevel = (LinearLayoutCompat) view.findViewById(R.id.layoutLevel);
+            layoutLevel = (LinearLayout) view.findViewById(R.id.layoutLevel);
             txtLevel = (AppCompatTextView) view.findViewById(R.id.txtLvl);
             bintang1 = (ImageView) view.findViewById(R.id.bintang1);
             bintang2 = (ImageView) view.findViewById(R.id.bintang2);
